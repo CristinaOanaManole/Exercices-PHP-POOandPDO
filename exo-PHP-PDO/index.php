627cbe5a3e7a7f7b797f6ef17e6b15e3ed5a2ede
CristinaOanaManole <oana_manole@yahoo.com>
2021-02-08 11:06:36 +0100

    sqlconnect

--- exo-PHP-PDO/index.php
+++ exo-PHP-PDO/index.php
@@ -1,12 +1,6 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-
-/* Connexion avec la base de donnée */
- $connexion = new PDO("mysql:host=$servername;dbname=sql_colyseum", $username, $password);
-
+require 'sqlconnect.php';
 
  ?>
 
---
+++ exo-PHP-PDO/sqlconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+/* Connexion avec la base de donnée */
+ $connexion = new PDO("mysql:host=$servername;dbname=sql_colyseum", $username, $password);
+
+
+ ?>
