b58fca41d959f23be36c913897a0ade8d1d20d8f
CristinaOanaManole <oana_manole@yahoo.com>
2021-02-08 10:53:54 +0100

    start pdo

---
+++ exo-PHP-PDO/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+/* Connexion avec la base de donnée */
+ $connexion = new PDO("mysql:host=$servername;dbname=sql_colyseum", $username, $password);
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="fr">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Partie 1 - PDO</title>
+ </head>
+ <body>
+     
+
+
+     
+ </body>
+ </html>
